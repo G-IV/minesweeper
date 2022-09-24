2701e04e8abc42a6299300e0dce4793c2aa44db2
George Burrows <unccmarine@gmail.com>
2022-09-24 12:11:17 -0500

    control displays are broken down into digits w/ 0 padding at start

---
+++ src/features/display/digit/digit.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Digit({props}){
+    return (
+        <div className="digit">
+            {props.val}
+        </div>
+    )
+}
---
+++ src/features/display/display.js
@@ -0,0 +1,20 @@
+import React, {} from "react";
+
+import Digit from "./digit/digit";
+import displayStyles from './display.module.css'
+
+export default function Display({props}){
+    const createDigit = () => {
+        console.log(JSON.stringify(props));
+        return String(props.val).padStart(props.max,'0').split('')
+    }
+    return (
+        <div className={displayStyles.display}>
+            {createDigit().map((digit, i) => {
+                return <div className="digit" key={`digitID_${i}`}>
+                    <Digit props={{val: digit}}/>
+                </div>
+            })}
+        </div>
+    )
+}
